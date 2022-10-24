b33e7ba698c963c4966fa482163c6c13f9692aeb
Jeferson Borges <jefersonmartinsborges@gmail.com>
2022-10-24 14:02:16 -0300

    fix: Trata string null

--- CleanArchMVC.Domain/Entities/Product.cs
+++ CleanArchMVC.Domain/Entities/Product.cs
@@ -37,13 +37,13 @@ namespace CleanArchMVC.Domain.Entities
             DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 3 characters.");
 
             DomainExceptionValidation.When(string.IsNullOrEmpty(description), "Invalid description. Description is required.");
-            DomainExceptionValidation.When(name.Length < 5, "Invalid name, too short, minimum 5 characters.");
+            DomainExceptionValidation.When(name.Length < 3, "Invalid name, too short, minimum 5 characters.");
 
             DomainExceptionValidation.When(price < 0, "Invalid price value.");
 
             DomainExceptionValidation.When(stock < 0, "Invalid stock value.");
 
-            DomainExceptionValidation.When(image.Length > 250, "Invalid image name, too long, maximum 250 characters.");
+            DomainExceptionValidation.When(image?.Length > 250, "Invalid image name, too long, maximum 250 characters.");
 
             Name = name;
             Description = description;
