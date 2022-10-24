b1b74ea3574e30e03c22237b626bd06034e6e25d
Jeferson Borges <jefersonmartinsborges@gmail.com>
2022-10-24 17:38:41 -0300

    feat: Adiciona configuração de entidades

---
+++ CleanArchMVC.Infra.Data/EntityConfiguration/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMVC.Infra.Data.EntityConfiguration
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(b => b.Id);
+            builder.Property(b => b.Name).HasMaxLength(100).IsRequired();
+
+            builder.HasData(
+                new Category(1, "Material Escolar"),
+                new Category(2, "Eletrônicos"),
+                new Category(3, "Acessórios"));
+        }
+    }
+}
