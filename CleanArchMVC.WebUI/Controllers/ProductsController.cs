7e675d8d82dcbd7add32237f91118929d66869f1
Jeferson Borges <jefersonmartinsborges@gmail.com>
2022-10-25 17:09:42 -0300

    feat: Adiciona ProductsController

---
+++ CleanArchMVC.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMVC.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productAppService)
+        {
+            _productService = productAppService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productService.GetProducts();
+            return View(products);
+        }
+    }
+}
