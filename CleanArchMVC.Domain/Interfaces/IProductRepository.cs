22821b57e1adb82557cc425350dc8e70938031ce
Jeferson Borges <jefersonmartinsborges@gmail.com>
2022-10-25 16:14:58 -0300

    feat: Adiciona DTOs e mapeamentos

--- CleanArchMVC.Domain/Interfaces/IProductRepository.cs
+++ CleanArchMVC.Domain/Interfaces/IProductRepository.cs
@@ -6,7 +6,6 @@ namespace CleanArchMVC.Domain.Interfaces
 {
     public interface IProductRepository
     {
-        Task<IEnumerable<Product>> GetCategoriesAsync();
         Task<Product> GetByIdAsync(int? id);
         Task<Product> GetProductCategoryAsync(int? id);
         Task<Product> CreateAsync(Product Product);
