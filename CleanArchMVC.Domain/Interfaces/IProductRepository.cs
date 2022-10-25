7a358b802a6d72b548ee85831767a5b40a063619
Jeferson Borges <jefersonmartinsborges@gmail.com>
2022-10-25 13:43:43 -0300

    feat: Adiciona repositório para Products

---
+++ CleanArchMVC.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMVC.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetCategoriesAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product Product);
+        Task<Product> UpdateAsync(Product Product);
+        Task<Product> RemoveAsync(Product Product);
+    }
+}
